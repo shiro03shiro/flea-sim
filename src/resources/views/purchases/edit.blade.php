c49f6b7f9bcc48b732e179408350297e683b4b8a
shiro <okano.03.remi@gmail.com>
2026-03-15 21:49:44 +0900

    住所変更ブレード

--- src/resources/views/purchases/edit.blade.php
+++ src/resources/views/purchases/edit.blade.php
@@ -1,18 +1,61 @@
 @extends('layouts.app')
 
+@section('css')
+<link rel="stylesheet" href="{{ asset('css/address.css') }}">
+@endsection
 
-<p>送付先住所変更画面</p>
-
-
-{{-- 配送先 --}}
-<div class="form__group">
-    <h3 class="form__group-title">配送先</h3>
-    <div class="form__group-content">
-        <div class="purchase__shipping-address">
-            <p>〒{{ auth()->user()->postal_code ?? '' }}</p>
-            <p>{{ auth()->user()->address ?? '' }}</p>
-        </div>
-
-        <p>配送先：{{ auth()->user()->address ?? '未登録' }}</p>
+@section('content')
+<div class="address-form__content">
+    <div class="address-form__heading">
+        <h2>住所の変更</h2>
     </div>
+    <form class="form" action="{{ route('profile.update') }}" method="post">
+        @method('PATCH')
+        @csrf
+        <div class="form__group">
+            <div class="form__group-title">
+                <span class="form__label--item">郵便番号</span>
+            </div>
+            <div class="form__group-content">
+                <div class="form__input--text">
+                <input type="text" name="postal_code" value="{{ old('postal_code', $profile->postal_code ?? '') }}" />
+                </div>
+                <div class="form__error">
+                @error('postal_code')
+                {{ $message }}
+                @enderror
+                </div>
+            </div>
+        </div>
+        <div class="form__group">
+            <div class="form__group-title">
+                <span class="form__label--item">住所</span>
+            </div>
+            <div class="form__group-content">
+                <div class="form__input--text">
+                <input type="text" name="address" value="{{ old('address', $profile->address ?? '') }}" />
+                </div>
+                <div class="form__error">
+                @error('address')
+                {{ $message }}
+                @enderror
+                </div>
+            </div>
+        </div>
+        <div class="form__group">
+            <div class="form__group-title">
+                <span class="form__label--item">建物名</span>
+            </div>
+            <div class="form__group-content">
+                <div class="form__input--text">
+                <input type="text" name="building" value="{{ old('building', $profile->building ?? '') }}" />
+                </div>
+            </div>
+            </div>
+        </div>
+        <div class="form__button">
+            <button class="form__button-submit" type="submit">更新する</button>
+        </div>
+    </form>
 </div>
+@endsection
