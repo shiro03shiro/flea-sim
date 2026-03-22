35feabbe73e4051da1403acb05ffe0cb92b3ba57
shiro <okano.03.remi@gmail.com>
2026-03-22 23:30:57 +0900

    コメントアイコン

--- src/resources/views/items/show.blade.php
+++ src/resources/views/items/show.blade.php
@@ -22,35 +22,39 @@
             <p class="item-detail__brand">{{ $item->brand_name ?? 'ブランド名なし' }}</p>
             <p class="item-detail__price">¥{{ number_format($item->price) }}(税込)</p>
         </div>
-
-        <div class="item-detail__like">
-            @if($item->user_id !== auth()->id())
-                @if(auth()->check() && $item->isLikedByAuthUser())
-                    <form action="{{ route('items.unlike', $item->id) }}" method="POST" style="display: inline;">
-                        @csrf @method('DELETE')
-                        <button type="submit" class="like-btn">
-                            <img src="{{ asset('images/ハートロゴ_ピンク.png') }}" width="30" height="30">
-                        </button>
-                    </form>
-                @else
-                    <form action="{{ route('items.like', $item->id) }}" method="POST" style="display: inline;">
-                        @csrf
-                        <button type="submit" class="like-btn">
-                            <img src="{{ asset('images/ハートロゴ_デフォルト.png') }}" width="30" height="30">
-                        </button>
-                    </form>
-                @endif
-                <span class="like-count">{{ $item->likes->count() }}</span>
-            @else
-                <div class="like-owner">
-                    <img src="{{ asset('images/ハートロゴ_デフォルト.png') }}" 
-                        class="like-icon--disabled" width="30" height="30">
-                    <span class="owner-message">あなたの出品商品です</span>
+        <div class="item-detail__meta-icons">
+            <div class="item-detail__like">
+                @if($item->user_id !== auth()->id())
+                    @if(auth()->check() && $item->isLikedByAuthUser())
+                        <form action="{{ route('items.unlike', $item->id) }}" method="POST" style="display: inline;">
+                            @csrf @method('DELETE')
+                            <button type="submit" class="like-btn">
+                                <img src="{{ asset('images/' . rawurlencode('ハートロゴ_ピンク.png')) }}" width="30" height="30">
+                            </button>
+                        </form>
+                    @else
+                        <form action="{{ route('items.like', $item->id) }}" method="POST" style="display: inline;">
+                            @csrf
+                            <button type="submit" class="like-btn">
+                                <img src="{{ asset('images/' . rawurlencode('ハートロゴ_デフォルト.png')) }}" width="30" height="30">
+                            </button>
+                        </form>
+                    @endif
                     <span class="like-count">{{ $item->likes->count() }}</span>
-                </div>
-            @endif
+                @else
+                    <div class="like-owner">
+                        <img src="{{ asset('images/' . rawurlencode('ハートロゴ_デフォルト.png')) }}" 
+                            class="like-icon--disabled" width="30" height="30">
+                        <span class="owner-message">あなたの出品商品です</span>
+                        <span class="like-count">{{ $item->likes->count() }}</span>
+                    </div>
+                @endif
+            </div>
+            <div class="item-detail__comment">
+                <img src="{{ asset('images/' . rawurlencode('ふきだしロゴ.png')) }}" width="30" height="30">
+                <span class="comment-count">{{ $item->comments->count() }}</span>
+            </div>
         </div>
-
         {{-- 購入ボタン --}}
         <div class="item-detail__action">
             @if($item->sold_flg)
