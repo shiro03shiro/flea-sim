036acc3532a293e5037188a0133dc81c2c83f588
shiro <okano.03.remi@gmail.com>
2026-03-28 13:59:48 +0900

    headerCSS仮

--- src/resources/views/layouts/app.blade.php
+++ src/resources/views/layouts/app.blade.php
@@ -14,45 +14,45 @@
 <body>
   <header class="header">
     <div class="header__inner">
-      <div class="header-utilities">
+      <div class="header__content">
         <a class="header__logo" href="/">
           <img src="{{ asset('images/COACHTECHヘッダーロゴ.png') }}" alt="COACHTECH">
         </a>
         @if (! in_array(Route::currentRouteName(), ['login', 'register']))
-          <form class="header__search-form" action="{{ route('home') }}" method="GET">
-            <input type="text" name="keyword" value="{{ request('keyword') }}" placeholder="なにをお探しですか？" class="header__search-input">
-            <button type="submit" class="header__search-button">検索</button>
+          <form class="header__search" action="{{ route('home') }}" method="GET">
+            <input class="header__search-input" type="text" name="keyword" value="{{ request('keyword') }}" placeholder="なにをお探しですか？">
+            <button class="header__search-button" type="submit">検索</button>
           </form>
-        @endif
 
-        <nav>
-          <ul class="header-nav">
-            @if (Auth::check())
-            <li class="header-nav__item">
-              <form class="form" action="/logout" method="post">
-                @csrf
-                <button class="header-nav__button">ログアウト</button>
-              </form>
-            </li>
-            <li class="header-nav__item">
-              <a class="header-nav__link" href="/mypage">マイページ</a>
-            </li>
-            <li class="header-nav__item">
-              <a class="header-nav__link" href="/sell">出品</a>
-            </li>
-            @else
-            <li class="header-nav__item">
-                <a class="header-nav__link" href="/login">ログイン</a>
-            </li>
-            <li class="header-nav__item">
-              <a class="header-nav__link" href="/mypage">マイページ</a>
-            </li>
-            <li class="header-nav__item">
-              <a class="header-nav__link" href="/sell">出品</a>
-            </li>
-            @endif
-          </ul>
-        </nav>
+          <nav class="header__nav">
+            <ul class="header__list">
+              @if (Auth::check())
+                <li class="header__item">
+                  <form class="form" action="/logout" method="post">
+                    @csrf
+                    <button class="header__button">ログアウト</button>
+                  </form>
+                </li>
+                <li class="header__item">
+                  <a class="header__link" href="/mypage">マイページ</a>
+                </li>
+                <li class="header__item">
+                  <a class="header__link" href="/sell">出品</a>
+                </li>
+              @else
+                <li class="header__item">
+                    <a class="header__link" href="/login">ログイン</a>
+                </li>
+                <li class="header__item">
+                  <a class="header__link" href="/mypage">マイページ</a>
+                </li>
+                <li class="header__item">
+                  <a class="header__link" href="/sell">出品</a>
+                </li>
+              @endif
+            </ul>
+          </nav>
+        @endif
       </div>
     </div>
   </header>
